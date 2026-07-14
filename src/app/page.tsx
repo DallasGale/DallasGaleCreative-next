567da53f22e75336c4acf145144b73d2d76b0959
Dallas Gale <dallasgale.digital@gmail.com>
2026-07-14 15:16:40 +1000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import BackgroundGradient from "@/components/BackgroundGradient"
 import Header from "@/components/Header"
 import Intro from "@/components/Intro"
-import About from "@/components/About"
 import RecentWork from "@/components/RecentWork"
 import Footer from "@/components/Footer"
 import ScrollEffects from "@/components/ScrollEffects"
@@ -13,7 +12,6 @@ export default function Home() {
       <Header />
       <main className="z-0 relative mx-auto flex flex-col p-5 xl:p-0">
         <Intro />
-        {/* <About /> */}
         <RecentWork />
 
         <h3 className="text-2xl md:text-8xl color-white font-extrabold text-center md:mb-20 h-50 mt-20">
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,8 +2,11 @@ import Employers from "@/components/Employers"
 
 export default function Intro() {
   return (
-    <section className="mx-auto mb-20 md:mb-50 flex min-h-[80vh] flex-col max-w-[1100px] items-start justify-center gap-10 pt-[140px] md:min-h-screen md:items-center md:pt-40 gap-50">
-      <div className="intro-blurb flex flex-col gap-10" id="intro-section">
+    <section className="mx-auto mb-20 md:mb-50 flex min-h-full flex-col max-w-[1100px] items-start justify-center gap-10  md:pt-0 md:items-center gap-50">
+      <div
+        className="intro-blurb flex flex-col gap-10 min-h-[60vh] md:min-h-[80dvh] flex items-center justify-center"
+        id="intro-section"
+      >
         <h2 className="text-[clamp(40px,17vw,120px)] font-extrabold leading-[0.8]">
           ...just a guy who designs and builds web stuff.
         </h2>
